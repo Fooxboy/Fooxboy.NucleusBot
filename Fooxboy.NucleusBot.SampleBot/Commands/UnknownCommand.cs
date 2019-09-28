c1d3c78fa763eb9ffa841df3b86aef565d34d880
Fooxboy <Fooxboy@yandex.ru>
2019-09-28 19:04:50 +0300

    Реализовал неизвестную команду

--- Fooxboy.NucleusBot.SampleBot/Commands/UnknownCommand.cs
+++ Fooxboy.NucleusBot.SampleBot/Commands/UnknownCommand.cs
@@ -1,11 +1,25 @@
 ﻿using Fooxboy.NucleusBot.Interfaces;
+using Fooxboy.NucleusBot.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Fooxboy.NucleusBot.SampleBot.Commands
 {
-    public class UnknownCommand:INucleusCommand
+    public class UnknownCommand : INucleusCommand
     {
+        public string Command => "unknown";
+
+        public string[] Aliases => null;
+
+        public void Execute(Message msg, IMessageSenderService sender, IBot bot)
+        {
+            sender.Text("Я не знаю такой команды, прости.", msg.ChatId);
+        }
+
+        public void Init(IBot bot, ILoggerService logger)
+        {
+            logger.Trace("Этот код вызывается при старте бота :)");
+        }
     }
 }
