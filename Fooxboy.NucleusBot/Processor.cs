7d867c3c8369d15832d9685bd6de1dfcf9188945
Fooxboy <Fooxboy@yandex.ru>
2019-10-05 10:32:20 +0300

    Фикс процессора

--- Fooxboy.NucleusBot/Processor.cs
+++ Fooxboy.NucleusBot/Processor.cs
@@ -55,7 +55,10 @@ namespace Fooxboy.NucleusBot
             if (command == null)
             {
                 var commandStr = string.Empty;
-                foreach(var alias in _bot.AliasesCommand) if (alias.Key.ToLower() == commandString.ToLower()) commandStr = alias.Value.ToLower();
+                foreach (var alias in _bot.AliasesCommand)
+                {
+                    if (alias.Key.ToLower() == commandString.ToLower()) commandStr = alias.Value.ToLower();
+                }
                 if (commandStr != string.Empty) command = _bot.Commands.Find(c => c.Command.ToLower() == commandStr.ToLower());
             }
             if (command == null) return _bot.UnknownCommand;
